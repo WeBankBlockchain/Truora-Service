9ed7d00a7d923896ab40cbbb6c29f0d7bd7187c3
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2020-12-17 15:55:50 +0800

    add ci

--- src/main/java/com/webank/oracle/history/ReqHistoryRepository.java
+++ src/main/java/com/webank/oracle/history/ReqHistoryRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 /**
  *
