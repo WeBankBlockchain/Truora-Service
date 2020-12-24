963e8d43431434b0123b793126053ce2a1ca9d61
yanyanho <yanyanho@126.com>
2020-12-24 17:39:29 +0800

    statisfy the import rule

--- src/main/java/com/webank/oracle/transaction/register/OracleRegisterCenterController.java
+++ src/main/java/com/webank/oracle/transaction/register/OracleRegisterCenterController.java
@@ -4,7 +4,12 @@ package com.webank.oracle.transaction.register;
 import com.webank.oracle.base.pojo.vo.BaseResponse;
 import com.webank.oracle.base.pojo.vo.ConstantCode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/center")
