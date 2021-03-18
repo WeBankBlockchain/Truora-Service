0bf19ffddcc7c64cb378b78523da4e29f4752a4f
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2021-03-18 10:27:59 +0800

    add actualSeed log

--- src/main/java/com/webank/oracle/transaction/vrf/VRFService.java
+++ src/main/java/com/webank/oracle/transaction/vrf/VRFService.java
@@ -14,6 +14,22 @@
 
 package com.webank.oracle.transaction.vrf;
 
+import static com.webank.oracle.base.enums.ReqStatusEnum.UPLOAD_RESULT_TO_CHAIN_ERROR;
+import static com.webank.oracle.base.enums.ReqStatusEnum.VRF_CONTRACT_ADDRESS_ERROR;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.fisco.bcos.web3j.crypto.Credentials;
+import org.fisco.bcos.web3j.protocol.Web3j;
+import org.fisco.bcos.web3j.protocol.core.DefaultBlockParameter;
+import org.fisco.bcos.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.fisco.bcos.web3j.utils.ByteUtil;
+import org.springframework.stereotype.Service;
+
 import com.webank.oracle.base.enums.ContractTypeEnum;
 import com.webank.oracle.base.exception.OracleException;
 import com.webank.oracle.base.pojo.vo.ConstantCode;
@@ -25,22 +41,8 @@ import com.webank.oracle.base.utils.ThreadLocalHolder;
 import com.webank.oracle.event.exception.FullFillException;
 import com.webank.oracle.event.service.AbstractCoreService;
 import com.webank.oracle.event.vo.BaseLogResult;
+
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
-import org.fisco.bcos.web3j.crypto.Credentials;
-import org.fisco.bcos.web3j.protocol.Web3j;
-import org.fisco.bcos.web3j.protocol.core.DefaultBlockParameter;
-import org.fisco.bcos.web3j.protocol.core.methods.response.TransactionReceipt;
-import org.fisco.bcos.web3j.utils.ByteUtil;
-import org.springframework.stereotype.Service;
-
-import java.math.BigInteger;
-import java.util.List;
-
-import static com.webank.oracle.base.enums.ReqStatusEnum.UPLOAD_RESULT_TO_CHAIN_ERROR;
-import static com.webank.oracle.base.enums.ReqStatusEnum.VRF_CONTRACT_ADDRESS_ERROR;
 
 /**
  * VRFService.
@@ -105,7 +107,7 @@ public class VRFService extends AbstractCoreService {
         Credentials credentials = keyStoreService.getCredentials();
         String servicePrivateKey = credentials.getEcKeyPair().getPrivateKey().toString(16);
 
-        log.info("Call vrf lib:[{}].", requestId);
+        log.info("Call vrf lib:[{}], actualSeed:[{}].", requestId, actualSeed);
         String proof = LibVRFK1.InstanceHolder.getInstance().prove(servicePrivateKey, actualSeed);
         log.info("Generate proof:[{}] for request:[{}]", proof, requestId);
 
