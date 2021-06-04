17c9e922e125a5c6370244fe9b1dc3665a16f5f4
dwusiq <dwusiq@163.com>
2021-06-04 14:23:58 +0800

    update: update contract

--- contracts/1.0/sol-0.4/BAC/blindbox/CatBlindbox.sol
+++ contracts/1.0/sol-0.4/BAC/blindbox/CatBlindbox.sol
@@ -11,41 +11,52 @@ import "../bac002/BAC002.sol";
 contract CatBlindbox is BAC002, Ownable {
     using SafeMath for uint256;
     event ResultOfNewBlindboxCat(address sender,string catName,bytes32 requestId);
-    event SurpriseCat(bytes32 requestId,string name,uint8 genes,uint256 birthTime,address owner,string images);
-//American Shorthair 美短   British Shorthair  英短   Japanese Bobtail 日本短尾猫   Chinese Orange 中华橘猫    Russian Blue 俄罗斯蓝猫    Persian 波斯猫   Ragdoll 布偶猫
-    enum CatSeries{ American_Shorthair, British_Shorthair, Japanese_Bobtail, Chinese_Orange, Russian_Blue, Persian, Ragdoll }
+    event SurpriseCat(bytes32 requestId,string name,string series,uint256 birthTime,address owner,string images);
 
-    string[7] catsNameDescription = ["American Shorthair", "British Shorthair", "Japanese Bobtail", "Chinese Orange", "Russian Blue", "Persian", "Ragdoll"];
+    //American Shorthair 美短   British Shorthair  英短   Japanese Bobtail 日本短尾猫   Chinese Orange 中华橘猫    Russian Blue 俄罗斯蓝猫    Persian 波斯猫   Ragdoll 布偶猫
+    string[7] catsSeries = ["American Shorthair", "British Shorthair", "Japanese Bobtail", "Chinese Orange", "Russian Blue", "Persian", "Ragdoll"];
 
-    string[] catImagesUri=["https://pic1.zhimg.com/80/v2-33f59a4343abb6a49b352789377ff5d8_1440w.jpg",
+    string[7] catImagesUri=["https://pic1.zhimg.com/80/v2-33f59a4343abb6a49b352789377ff5d8_1440w.jpg",
                           "https://pic2.zhimg.com/80/v2-6838d36654876924d75e8cc11545399d_1440w.jpg",
                           "https://pic1.zhimg.com/80/v2-25e8bb0321b78da95990bcdedb692c0c_1440w.jpg",
                             "https://pic1.zhimg.com/v2-82c6c9ea312eb5eeb93d7f53c8e3259a_1440w.jpg",
                             "https://pic1.zhimg.com/80/v2-ce3a26ff6f96c222a9ee93d8179328dc_1440w.jpg",
                             "https://pic4.zhimg.com/80/v2-7a4f80d774519b1cf4be4bcfd5641427_1440w.jpg",
                             "https://pic1.zhimg.com/80/v2-f8d012397a26872df456291ae1772b58_1440w.jpg"];
+
+    struct CatType{
+        string series;
+        string images;
+    }
+
     struct Cat {
+        uint256 catId;
         string name;
-        uint8 genes;
+        string series;
         uint256 birthTime;
         address owner;
         string images;
     }
 
-    Cat[] public cats;
+
+    CatType[] catTypePool;//猫池，所有盲盒中，还剩下的猫类型
+    Cat[] public generatedCats;//已经产生的猫
     mapping(bytes32 => string)  requestToCatName;
     mapping(bytes32 => address) requestToSender;
     RandomNumberSampleVRF private oracle;
 
     constructor(address randomOracle) public BAC002("CatBlindBoxSeries", "Cat"){
         oracle = RandomNumberSampleVRF(randomOracle);
+        for(uint i=0;i<catsSeries.length;i++){
+           catTypePool.push(CatType(catsSeries[i],catImagesUri[i]));
+        }
     }
 
     function requestNewBlindboxCat(
         uint256 userProvidedSeed,
         string  name
     ) public returns (bytes32) {
-        require(catImagesUri.length >0 , "There are no blind box cats!");
+        require(catTypePool.length >0 , "There are no blind box cats!");
         bytes32  requestId = oracle.getRandomNumber(userProvidedSeed);
         requestToCatName[requestId] = name;
         requestToSender[requestId] = msg.sender;
@@ -55,42 +66,44 @@ contract CatBlindbox is BAC002, Ownable {
 
 
     function generateBlindBoxCat(bytes32 requestId) public {
-        require(catImagesUri.length >0 , "There are no blind box cats!");
+        require(catTypePool.length >0 , "There are no blind box cats!");
         require(oracle.checkIdFulfilled(requestId) == false, " oracle query has not been fulfilled!");
         uint256 randomness  = oracle.getById(requestId);
-        uint8 index = uint8((randomness) % catImagesUri.length);
+        uint8 index = uint8((randomness) % catTypePool.length);
         string name =  requestToCatName[requestId];
         // string  images = catImagesUri[index];
-        string memory images = _podCatImage(index);
-        uint256 birthTime = now;
-        cats.push(Cat(name, index, birthTime, requestToSender[requestId], images));
-        issueWithAssetURI(requestToSender[requestId], cats.length, images, "");
-        emit SurpriseCat(requestId, name,index, birthTime,requestToSender[requestId], images);
+        CatType memory catType = _removeAndPopFromCatTypePoolByIndex(index);
+        uint256 birthTime = block.timestamp;
+        uint256 nftId = generatedCats.length;
+        generatedCats.push(Cat(nftId,name, catType.series, birthTime, requestToSender[requestId], catType.images));
+        issueWithAssetURI(requestToSender[requestId], nftId, catType.images, "");
+        emit SurpriseCat(requestId, name,catType.series, birthTime,requestToSender[requestId], catType.images);
     }
 
 
     function getCatInfo(uint256 catId)
     public
     view
-    returns (string , uint8 , uint256 , address , string )
+    returns (uint256,string,string, uint256 , address , string )
     {
-        return (
-        cats[catId].name,
-        cats[catId].genes,
-        cats[catId].birthTime,
-        cats[catId].owner,
-        cats[catId].images);
+        Cat memory cat = generatedCats[catId];
+            return (cat.catId,
+                    cat.name,
+                    cat.series,
+                    cat.birthTime,
+                    cat.owner,
+                    cat.images);
     }
 
-    function _podCatImage(uint8 index)internal returns(string){
-        string dataByIndex = catImagesUri[index];
-        string lastElement = catImagesUri[catImagesUri.length-1];
-        delete catImagesUri[catImagesUri.length - 1];
-        catImagesUri.length = catImagesUri.length-1;
+    function _removeAndPopFromCatTypePoolByIndex(uint8 index)internal returns(CatType memory){
+        CatType memory dataByIndex = catTypePool[index];
+        CatType memory lastElement = catTypePool[catTypePool.length-1];
+        delete catTypePool[catTypePool.length - 1];
+        catTypePool.length = catTypePool.length-1;
         return dataByIndex;
     }
 
     function getLength()public view returns(uint256){
-        return catImagesUri.length;
+        return catTypePool.length;
     }
 }
